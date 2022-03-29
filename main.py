a2373b842d5495d6d6e76d20299bd5cdcc8d65fe
thulanimbatha <thulanimbatha06@gmail.com>
2022-03-29 20:30:48 +0200

    added player to gameplay

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from turtle import Screen
 import time
+from player import Player
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+player = Player()
 
 game_is_on = True
 while game_is_on:
--- player.py
+++ player.py
@@ -1,6 +1,14 @@
+from turtle import Turtle
+
 STARING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
-class Player:
-    pass
+class Player(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")    # set shape
+        self.penup()    # penup
+        self.goto(STARING_POSITION) # go to the starting position
+        self.setheading(90) # face north
