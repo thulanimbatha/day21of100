ca50896121128c0aa0b7fab2b5299d9d968133a1
thulanimbatha <thulanimbatha06@gmail.com>
2022-04-01 20:55:04 +0200

    added car speed functionality

--- player.py
+++ player.py
@@ -10,9 +10,20 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")    # set shape
         self.penup()    # penup
-        self.goto(STARING_POSITION) # go to the starting position
+        self.go_to_start() # go to the starting position
         self.setheading(90) # face north
 
     # move the player forward
     def go_up(self):
         self.forward(MOVE_DISTANCE)
+
+    # reset player position
+    def go_to_start(self):
+        self.goto(STARING_POSITION)
+
+    # successful crossing?
+    def is_at_finish_line(self):
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
