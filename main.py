0984de0f02ad9f171d014a7d356e6f3342bc7680
thulanimbatha <thulanimbatha06@gmail.com>
2022-04-01 20:40:54 +0200

    added car collision functionality

--- main.py
+++ main.py
@@ -20,3 +20,11 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    # detect collision with cars
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
+
